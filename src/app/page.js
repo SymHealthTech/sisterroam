54b8cf4b6fc7f7a27198e187a1b2b6855d8956a6
sat-tish <satish30yash@gmail.com>
2026-04-26 16:55:14 +0530

    corrections

--- src/app/page.js
+++
@@ -1,6 +0,0 @@
-// The homepage lives in (public)/page.js.
-// This file must be deleted (or this project moved to use only one route group)
-// to avoid the "Two pages resolve to the same URL" Next.js build error.
-// See: src/app/(public)/page.js
-
-export { default, metadata } from './(public)/page'
